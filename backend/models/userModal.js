95197817a4c9422cd94c3bf48f6a6f4eb775ac98
Adarsh Shukla <adarsh.04082002@gmail.com>
2024-01-10 00:49:25 +0530

    modals

---
+++ backend/models/userModal.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name:{type:String,required:true},
+    email:{type:String,required:true},
+    password:{type:String,required:true},
+    pic:{
+        type:String,
+        required:true,
+        default:"https://img.freepik.com/premium-vector/user-profile-icon-flat-style-member-avatar-vector-illustration-isolated-background-human-permission-sign-business-concept_157943-15752.jpg"
+    },
+
+});
+
+const User = mongoose.model("User",userSchema);
+
+module.exports = User;
