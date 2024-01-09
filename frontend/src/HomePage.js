1976006690ab0f39d76046fb6ef41f5693017100
Adarsh Shukla <adarsh.04082002@gmail.com>
2024-01-10 01:21:23 +0530

    login signup page UI started

--- frontend/src/HomePage.js
+++ frontend/src/HomePage.js
@@ -1,9 +1,34 @@
+import { Box, Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
 import React from "react";
+import { Login } from "./components/Authenticatons/Login";
+import { SignUp } from "./components/Authenticatons/SignUp";
 
 const HomePage = ()=>{
     return(
         <>
-        Hello
+       <Box
+       d="flex"
+       justifyContent="center"
+       alignItems="center"
+       w="50%"
+       margin="0px auto"
+       borderWidth="1px"
+       >
+       <Tabs variant='soft-rounded' colorScheme='green'>
+  <TabList>
+    <Tab width="50%">Login</Tab>
+    <Tab width="50%">Sign Up</Tab>
+  </TabList>
+  <TabPanels>
+    <TabPanel>
+     <Login/>
+    </TabPanel>
+    <TabPanel>
+     <SignUp/>
+    </TabPanel>
+  </TabPanels>
+</Tabs>
+       </Box>
         </>
     )
 }
