0da86aed531697e9cd66c04473c9a8f81ff3ac0e
Adarsh Shukla <adarsh.04082002@gmail.com>
2024-01-12 01:08:06 +0530

    chat API

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { protect } = require('../middleware/authMiddleware');
-
+const {accesChat,fetchChats} = require('../controller/chatController')
 const router = express.Router();
 
-// router.route('/').post(protect,accesChat);
-// router.route('/').get(protect,fetchChats);
+router.route('/').post(protect,accesChat);
+router.route('/').get(protect,fetchChats);
 // router.route('/group').post(protect,createGroupChat);
 // router.route('/rename').put(protect,renameChat);
 // router.route('/groupRemove').post(protect,removeFromGroup);
