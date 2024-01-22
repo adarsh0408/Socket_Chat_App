8cc2e9a73a0ec4b640fb836e040eed6897eae438
Adarsh Shukla <adarsh.04082002@gmail.com>
2024-01-22 19:52:17 +0530

    update

--- frontend/src/components/mislleneous/GroupChatModal.js
+++ frontend/src/components/mislleneous/GroupChatModal.js
@@ -12,15 +12,17 @@ import {
   useToast,
   FormControl,
   Input,
+  Box,
 } from "@chakra-ui/react";
 import { ChatState } from "../../Context/ChatProvider";
 import axios from "axios";
 import UserListItem from "../UserAvtar/UserListItem";
+import UserBadgeItem from "../UserAvtar/UserBadgeItem";
 
 const GroupChatModal = ({ children }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [groupChatName, setgroupChatName] = useState();
-  const [selectedUser, setSelectedUser] = useState();
+  const [selectedUser, setSelectedUser] = useState([]);
   const [search, setsearch] = useState("");
   const [searchResult, setsearchResult] = useState([]);
   const [loading, setloading] = useState(false);
@@ -29,36 +31,57 @@ const GroupChatModal = ({ children }) => {
 
   const { user, chats, setChats } = ChatState();
 
-  const handleSearch = async(query) => {
+  const handleSearch = async (query) => {
     setsearch(query);
-    if(!query){
-      return
+    if (!query) {
+      return;
     }
     try {
       setloading(true);
 
-      const config ={
-        headers:{
-          "Content-type":"application/json",
-          Authorization:`Bearer ${user.token}`
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${user.token}`,
         },
-      }
-      const {data} = await axios.get(`/api/user?search=${search}`,config);
+      };
+      const { data } = await axios.get(`/api/user?search=${search}`, config);
       console.log(data);
       setloading(false);
       setsearchResult(data);
     } catch (error) {
       toast({
-        title:"Error Occured!",
-        description:"Failed to load the chat result",
-        status:"error",
-        isClosable:true,
-        position:"top-right",
-        duration:3000
-      })
+        title: "Error Occured!",
+        description: "Failed to load the chat result",
+        status: "error",
+        isClosable: true,
+        position: "top-right",
+        duration: 3000,
+      });
     }
   };
   const handleSubmit = () => {};
+  
+  const handleDelete = (delUser) => {
+    console.log(delUser);
+    setSelectedUser(selectedUser.filter((sel)=>sel._id !== delUser._id))
+  };
+
+  const handleGroup = (userToAdd) => {
+    if (selectedUser.includes(userToAdd)) {
+      toast({
+        title: "User Already Exist",
+        // description:"Failed to load the chat result",
+        status: "warning",
+        isClosable: true,
+        position: "top-right",
+        duration: 3000,
+      });
+      return;
+    }
+
+    setSelectedUser([...selectedUser, userToAdd]);
+  };
   return (
     <>
       <span onClick={onOpen}>{children}</span>
@@ -84,11 +107,29 @@ const GroupChatModal = ({ children }) => {
                 onChange={(e) => handleSearch(e.target.value)}
               />
             </FormControl>
-            {/* Selected Users */}
-            {loading ? <div>loading</div> : (
-              searchResult?.slice(0,4).map(user=>(
-                <UserListItem/>
-              ))
+          <Box width="100%" display="flex" flexWrap="wrap">
+
+            {selectedUser?.map((u) => (
+              <UserBadgeItem
+              key={user._id}
+              user={u}
+              handleFunction={() => handleDelete(u)}
+              />
+              ))}
+              </Box>
+
+            {loading ? (
+              <div>loading</div>
+            ) : (
+              searchResult
+                ?.slice(0, 4)
+                .map((user) => (
+                  <UserListItem
+                    key={user._id}
+                    user={user}
+                    handleFunction={() => handleGroup(user)}
+                  />
+                ))
             )}
           </ModalBody>
 
